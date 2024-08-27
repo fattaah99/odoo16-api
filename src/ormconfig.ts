c9ae14d673bcd6a9a57063e2fc41b5fe4f12d594
fattaah99 <fattaah99@gmail.com>
2024-08-27 14:43:27 +0700

    update ormconfig and .env

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,24 +1,3 @@
-// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-// import { config } from 'dotenv';
-// import { Attendance } from './attendance/attendance.entity';
-// import { Employee } from './employee/employee.entity';
-// // import { User } from './Auth/auth.entity';
-
-// config();
-
-// export const typeOrmConfig: TypeOrmModuleOptions = {
-//   type: process.env.DB_TYPE as any,
-//   host: process.env.DB_HOST,
-//   port: parseInt(process.env.DB_PORT, 10),
-//   username: process.env.DB_USERNAME,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_DATABASE,
-//   // entities: [Attendance, Employee, User],
-//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-//   synchronize: false,
-//   logging: false,
-// };
-
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from 'dotenv';
 import { Attendance } from './attendance/attendance.entity';
@@ -27,16 +6,14 @@ import { Employee } from './employee/employee.entity';
 
 config();
 
-const databaseUrl = process.env.DATABASE_URL;
-const databaseType = process.env.DB_TYPE;
-
-if (!databaseUrl) {
-  throw new Error('DATABASE_URL is not defined in the environment variables');
-}
-
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  url: databaseUrl,
+  type: process.env.DB_TYPE as any,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  // entities: [Attendance, Employee, User],
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
   logging: false,
@@ -49,3 +26,34 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     },
   },
 };
+
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// import { config } from 'dotenv';
+// import { Attendance } from './attendance/attendance.entity';
+// import { Employee } from './employee/employee.entity';
+// // import { User } from './Auth/auth.entity';
+
+// config();
+
+// const databaseUrl = process.env.DATABASE_URL;
+// const databaseType = process.env.DB_TYPE;
+
+// if (!databaseUrl) {
+//   throw new Error('DATABASE_URL is not defined in the environment variables');
+// }
+
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   url: databaseUrl,
+//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//   synchronize: false,
+//   logging: false,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+//   extra: {
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   },
+// };
